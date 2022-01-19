082c145bdeab0464f037075492b82b0972bb5c80
JFJAX <93730265+JFJAX@users.noreply.github.com>
2022-01-19 11:11:40 -0600

    Update constants.rb

--- lib/constants.rb
+++ lib/constants.rb
@@ -5,8 +5,8 @@ module Constants
   PRE_MEETINGS = 0
   PRE_HOURS = 0
   DEFAULT_LOGIN = "roboticsteam.crhs1@gmail.com"
-  TEAM_NAME = 'C.A.R.D.S.'
-  TEAM_NUMBER = '7850'
+  TEAM_NAME = 'FRC Team 7850 - C.A.R.D.S.'
+  TEAM_NUMBER = ''
   SCHOOL_NAME_REPLACEMENT = 'Sub-Team'
   GENDERS = ['Male', 'Female', 'Non-Binary']
   FRESHMAN_HOURS = 0
